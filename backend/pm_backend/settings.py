56f4ba542165ce923ce2d2fe6c845cfe7b715e9b
Anuj Kumar Verma <anujvk@outlook..com>
2025-08-13 08:25:53 +0530

    Add

--- backend/pm_backend/settings.py
+++ backend/pm_backend/settings.py
@@ -88,6 +88,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
