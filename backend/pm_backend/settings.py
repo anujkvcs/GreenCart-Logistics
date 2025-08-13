6560b98ee7cd7ca388ae1eea4755a0eaadaab175
Anuj Kumar Verma <anujvk@outlook..com>
2025-08-13 11:28:27 +0530

    Fix CORS settings for Vercel domain

--- backend/pm_backend/settings.py
+++ backend/pm_backend/settings.py
@@ -93,3 +93,20 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://green-cart-logistic.vercel.app",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
